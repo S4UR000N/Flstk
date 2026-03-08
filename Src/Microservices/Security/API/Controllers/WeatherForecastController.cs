1935781252dec07b79d3c2fc84717e734d47b870
Robert Aščić <robert.ascic20@gmail.com>
2026-03-08 17:50:01 +0100

    Install PostgreSQL, Keycloak, Security.API and compose.yaml

---
+++ Src/Microservices/Security/API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Security.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<int> Get()
+        {
+            return Enumerable.Range(1, 5);
+        }
+    }
+}
